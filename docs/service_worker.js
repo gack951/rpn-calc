2be0308b3c15723b74273174984f4a212205414c
gack951 <gack951@gmail.com>
2021-03-07 18:47:51 +0900

    add fetch first

--- docs/service_worker.js
+++ docs/service_worker.js
@@ -6,17 +6,6 @@ var urlsToCache = [
     '/style.css',
 ];
 
-// インストール処理
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
 // リソースフェッチ時のキャッシュロード処理
 self.addEventListener('fetch', function(event) {
     event.respondWith(
@@ -24,6 +13,17 @@ self.addEventListener('fetch', function(event) {
             .match(event.request)
             .then(function(response) {
                 return response ? response : fetch(event.request);
+            })
+    );
+});
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
             })
     );
 });
