65e85b1eba19d95c21212cfa58904ead6aa42fa3
gack951 <gack951@gmail.com>
2021-03-07 16:38:19 +0900

    short vibration

--- docs/script.js
+++ docs/script.js
@@ -2,6 +2,6 @@ window.onload = ()=>{
     $("button").on({"touchstart":(e)=>$(e.target).addClass("hover"), "touchend":(e)=>$(e.target).removeClass("hover")});
 
     $("button.vibrate1").on('touchstart',function(){
-        navigator.vibrate(100);
+        navigator.vibrate(50);
     });
 };
