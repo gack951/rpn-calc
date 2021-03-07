a26ea33c284dd54a2c7f982ddbda8581853e56bf
gack951 <gack951@gmail.com>
2021-03-07 17:47:59 +0900

    PWA

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,10 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service_worker.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+    });
+}
 window.onload = ()=>{
     $("button").on({"touchstart":(e)=>$(e.target).addClass("hover"), "touchend":(e)=>$(e.target).removeClass("hover")});
 
