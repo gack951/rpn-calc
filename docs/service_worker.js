91881d611ff2524a6a6ce3ef0d6e5b72015022a9
gack951 <gack951@gmail.com>
2021-03-07 18:49:31 +0900

    revert

--- docs/service_worker.js
+++ docs/service_worker.js
@@ -6,17 +6,6 @@ var urlsToCache = [
     '/style.css',
 ];
 
-// リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
-    );
-});
-
 // インストール処理
 self.addEventListener('install', function(event) {
     event.waitUntil(
@@ -24,6 +13,17 @@ self.addEventListener('install', function(event) {
             .open(CACHE_NAME)
             .then(function(cache) {
                 return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
             })
     );
 });
